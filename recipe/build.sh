fa198515eded85aa69afb8ba8bc327c451fe8c81
vkhomits <khomitsvalia@gmail.com>
2026-02-10 17:20:07 +0200

    Update version to 9.0.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,25 +2,36 @@
 
 set -euxo pipefail
 
+if [[ "${target_platform}" == osx-* ]]; then
+    # See also https://gitlab.kitware.com/cmake/cmake/-/issues/25755
+    export CFLAGS="${CFLAGS} -fno-define-target-os-macros -target ${HOST}"
+    export LDFLAGS="${LDFLAGS} -target ${HOST}"
+fi
+
 source gen-bazel-toolchain
 
+export ABSEIL_VERSION=$(conda list -p $PREFIX libabseil | grep -v '^#' | tr -s ' ' | cut -f 2 -d ' ')
 export PROTOC_VERSION=$(conda list -p $PREFIX libprotobuf | grep -v '^#' | tr -s ' ' | cut -f 2 -d ' ' | sed -E 's/^[0-9]+\.([0-9]+\.[0-9]+)$/\1/')
-export PROTOBUF_JAVA_MAJOR_VERSION="4"
+export PROTOBUF_JAVA_MAJOR_VERSION="3"
 export EXTRA_BAZEL_ARGS="--host_javabase=@local_jdk//:jdk"
-sed -ie "s:PROTOC_VERSION:${PROTOC_VERSION}:" WORKSPACE
-sed -ie "s:PROTOBUF_JAVA_MAJOR_VERSION:${PROTOBUF_JAVA_MAJOR_VERSION}:" WORKSPACE
 sed -ie "s:\${INSTALL_NAME_TOOL}:${INSTALL_NAME_TOOL:-install_name_tool}:" src/BUILD
 sed -ie "s:\${PREFIX}:${PREFIX}:" src/BUILD
 sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/grpc/BUILD
 sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/systemlibs/protobuf/BUILD
+sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/systemlibs/protobuf/src/google/protobuf/compiler/BUILD
 sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/ijar/BUILD
+sed -ie "s:ABSEIL_VERSION:${ABSEIL_VERSION}:" third_party/systemlibs/protobuf/MODULE.bazel
+sed -ie "s:ABSEIL_VERSION:${ABSEIL_VERSION}:" MODULE.bazel
 
-chmod +x bazel
+cp -ap $PREFIX/share/bazel/protobuf/bazel third_party/systemlibs/protobuf/
+
+chmod +x bazel-${PKG_VERSION}
 pushd src/tools/singlejar
-../../../bazel build \
+../../../bazel-${PKG_VERSION} build \
 	--logging=6 \
 	--subcommands \
 	--verbose_failures \
+	--noincompatible_enable_proto_toolchain_resolution \
 	--define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include \
 	--extra_toolchains=//bazel_toolchain:cc_cf_toolchain \
 	--extra_toolchains=//bazel_toolchain:cc_cf_host_toolchain \
