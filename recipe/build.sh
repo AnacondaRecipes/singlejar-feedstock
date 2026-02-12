69658ba96a26606c1ee6f49c4282b44751419efd
vkhomits <khomitsvalia@gmail.com>
2026-02-12 16:20:01 +0200

    Update version to 8.3.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,8 @@ set -euxo pipefail
 
 if [[ "${target_platform}" == osx-* ]]; then
     # See also https://gitlab.kitware.com/cmake/cmake/-/issues/25755
-    export CFLAGS="${CFLAGS} -fno-define-target-os-macros"
+    export CFLAGS="${CFLAGS} -fno-define-target-os-macros -target ${HOST}"
+    export LDFLAGS="${LDFLAGS} -target ${HOST}"
 fi
 
 source gen-bazel-toolchain
@@ -17,19 +18,21 @@ sed -ie "s:\${INSTALL_NAME_TOOL}:${INSTALL_NAME_TOOL:-install_name_tool}:" src/B
 sed -ie "s:\${PREFIX}:${PREFIX}:" src/BUILD
 sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/grpc/BUILD
 sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/systemlibs/protobuf/BUILD
+sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/systemlibs/protobuf/src/google/protobuf/compiler/BUILD
 sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/ijar/BUILD
 sed -ie "s:ABSEIL_VERSION:${ABSEIL_VERSION}:" third_party/systemlibs/protobuf/MODULE.bazel
 sed -ie "s:ABSEIL_VERSION:${ABSEIL_VERSION}:" MODULE.bazel
 
 cp -ap $PREFIX/share/bazel/protobuf/bazel third_party/systemlibs/protobuf/
 
-chmod +x bazel
+chmod +x bazel-${PKG_VERSION}
 pushd src/tools/singlejar
-../../../bazel build \
+../../../bazel-${PKG_VERSION} build \
 	--logging=6 \
 	--subcommands \
 	--verbose_failures \
 	--define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include \
+	--noincompatible_enable_proto_toolchain_resolution \
 	--extra_toolchains=//bazel_toolchain:cc_cf_toolchain \
 	--extra_toolchains=//bazel_toolchain:cc_cf_host_toolchain \
 	--platforms=//bazel_toolchain:target_platform \
